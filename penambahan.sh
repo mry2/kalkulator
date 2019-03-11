52ed6b57743145a4a9a302dddb81f0b327d4ffda
mry2 <48435327+mry2@users.noreply.github.com>
2019-03-11 19:55:36 +0700

    Create penambahan.sh

---
+++ penambahan.sh
@@ -0,0 +1,25 @@
+#!/system/xbin/bash
+clear
+figlet Penambahan| lolcat
+echo "masukkan angka pertama"
+read p
+echo "masukkan angka kedua"
+read q
+tambah=$(echo "$p + $q" | bc)
+echo "hasil penambahan = $tambah"
+echo "\033[31;1m00. Ulangi"
+echo "\033[31;1m99. Kembali"
+
+read mrrm
+
+if
+[ $mrrm = 00 ] || [ $mrrm = 00 ]
+then
+sh penambahan.sh
+fi
+
+if
+[ $mrrm = 99 ] || [ $mrrm = 99 ]
+then
+sh kalkulator.sh
+fi
